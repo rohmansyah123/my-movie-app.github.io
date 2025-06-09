fb32d8612daa461bce8a72df41823d1563127821
ROHMANSYAH <40875901+rohmansyah123@users.noreply.github.com>
2025-06-09 17:51:12 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@
 // Ini hanya untuk tujuan demonstrasi sederhana. Untuk aplikasi produksi, gunakan server-side proxy
 // atau environment variables yang aman.
 
-const API_KEY = 'YOUR_API_KEY'; // Ganti dengan API Key TMDb v3 Anda
+const API_KEY = '6b2dec73b6697866a50cdaef60ccffcb'; // Ganti dengan API Key TMDb v3 Anda
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
 const YOUTUBE_EMBED_URL = 'https://www.youtube.com/embed/'; // URL dasar untuk embed YouTube
