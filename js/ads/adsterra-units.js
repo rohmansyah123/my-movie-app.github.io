a2e4c546433a4f8d502264af897801df8346946e
ROHMANSYAH <40875901+rohmansyah123@users.noreply.github.com>
2025-06-10 01:17:18 +0700

    Update adsterra-units.js

--- js/ads/adsterra-units.js
+++ js/ads/adsterra-units.js
@@ -19,7 +19,7 @@ export function loadBannerAd(containerId) {
     // --- TEMPATKAN KODE ADSTERRA BANNER ANDA DI SINI ---
     // Contoh placeholder:
     container.innerHTML = `
-        <div id="container-${2c979ea6eea470e28aecac661089d1a9}-ad"></div>
+        <div id="container-2c979ea6eea470e28aecac661089d1a9-ad"></div>
         <script async data-cfasync="false" src="//pl26583030.profitableratecpm.com/2c979ea6eea470e28aecac661089d1a9/invoke.js"></script>
     `;
     // Ganti "//pl22168345.topcreativeformat.com/YOUR_ACTUAL_ADSTERRA_BANNER_CODE_ID/invoke.js"
